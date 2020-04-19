19da11a49cbc04ad3bb9157e1effb37f49aab877
dheera <dheera@dheera.net>
2020-04-19 22:09:08 +0000

    browser

--- browser-samples/imuNode.js
+++ browser-samples/imuNode.js
@@ -9,53 +9,64 @@
   let pub_imu_data = nh.advertise("/imu/data", "sensor_msgs/Imu");
   let msg = { "@type": "sensor_msgs/Imu" };
 
-  let gyroscopeSensor = new Gyroscope({
-    frequency: 30,
-    referenceFrame: "device"
-  });
+  let gyroscopeSensor = null;
+  if(window.Gyroscope) {
+    gyroscopeSensor = new Gyroscope({
+      frequency: 30,
+      referenceFrame: "device"
+    });
+    gyroscopeSensor.addEventListener("reading", e => {
+      msg.angular_velocity = {
+        x: gyroscopeSensor.x,
+        y: gyroscopeSensor.y,
+        z: gyroscopeSensor.z,
+      };
+    });
+    gyroscopeSensor.start();
+  } else {
+    console.log("imu_node: Gyroscope not found");
+  }
 
-  let linearAccelerationSensor = new LinearAccelerationSensor({
-    frequency: 30,
-    referenceFrame: "device"
-  });
+  let linearAccelerationSensor = null;
+  if(window.LinearAccelerationSensor) {
+    linearAccelerationSensor = new LinearAccelerationSensor({
+      frequency: 30,
+      referenceFrame: "device"
+    });
+    linearAccelerationSensor.addEventListener("reading", e => {
+      msg.linear_acceleration = {
+        x: linearAccelerationSensor.x,
+        y: linearAccelerationSensor.y,
+        z: linearAccelerationSensor.z,
+      };
+    });
+    linearAccelerationSensor.start();
+  } else {
+    console.log("imu_node: LinearAccelerationSensor not found");
+  }
 
-  let orientationSensor = new AbsoluteOrientationSensor({
-    frequency: 30,
-    referenceFrame: "device"
-  });
-
-  gyroscopeSensor.addEventListener("reading", e => {
-    msg.angular_velocity = {
-      x: gyroscopeSensor.x,
-      y: gyroscopeSensor.y,
-      z: gyroscopeSensor.z,
-    };
-  });
-
-  linearAccelerationSensor.addEventListener("reading", e => {
-    msg.linear_acceleration = {
-      x: linearAccelerationSensor.x,
-      y: linearAccelerationSensor.y,
-      z: linearAccelerationSensor.z,
-    };
-  });
-
-  orientationSensor.addEventListener("reading", e => {
-    msg.orientation = {
-      x: orientationSensor.quaternion[0],
-      y: orientationSensor.quaternion[1],
-      z: orientationSensor.quaternion[2],
-      w: orientationSensor.quaternion[3],
-    };
-    msg.header = {
-      timestamp: orientationSensor.timestamp,
-      frame_id: "imu",
-    };
-    pub_imu_data.publish(msg);
-  });
-
-  gyroscopeSensor.start();
-  linearAccelerationSensor.start();
-  orientationSensor.start();
+  let orientationSensor = null;
+  if(window.AbsoluteOrientationSensor) {
+    orientationSensor = new AbsoluteOrientationSensor({
+      frequency: 30,
+      referenceFrame: "device"
+    });
+    orientationSensor.addEventListener("reading", e => {
+      msg.orientation = {
+        x: orientationSensor.quaternion[0],
+        y: orientationSensor.quaternion[1],
+        z: orientationSensor.quaternion[2],
+        w: orientationSensor.quaternion[3],
+      };
+      msg.header = {
+        timestamp: orientationSensor.timestamp,
+        frame_id: "imu",
+      };
+      pub_imu_data.publish(msg);
+    });
+    orientationSensor.start();
+  } else {
+    console.log("imu_node: AbsoluteOrientationSensor not found");
+  }
 
 })();
